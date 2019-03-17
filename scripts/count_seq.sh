d7d334951b46a53205dd2cc6fbc95260a35c6ac4
Deborah Spikes <deborah.spikes@stonybrook.edu>
2019-03-17 10:20:53 -0400

    feat: runs with any FASTA minor doc change

--- scripts/count_seq.sh
+++ scripts/count_seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # USAGE: bash count_seq.sh
-# Description: count number of seqa in a genome
+# Description: script to count number of seqa in a genome
 
-grep -c ">" ../fasta/genomes.fa
+grep -c ">" ../$1
